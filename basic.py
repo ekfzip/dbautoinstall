3b9599f1813061ebde57f379afcb790c6a03e7d1
ekfzip <ekf3619@gmail.com>
2024-12-27 17:14:08 +0900

    ssh 통신 관련 함수 세분화

--- basic.py
+++ basic.py
@@ -5,23 +5,20 @@ import logging
 logger = logging.getLogger()
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    
-    return "hello"
-
 @app.route('/install', methods=['POST'])    
 def executeProcess():
+    
     
     data = request.get_json()
     
     checkValidation(data)
-    list = getData(data=data)
     
-    for i in list:
-        print(i)
+    list = getData(data)
+    
+    ssh_master, ssh_slave1, ssh_slave2 = testSSHConnection(data)
         
-    sshCommand(list)
+    sshCommand(ssh_master, ssh_slave1, ssh_slave2, list)
+    
     setLogFile()
 
     return "finished process"
@@ -33,6 +30,7 @@ def checkValidation(data):
     return "successfully received data"
     
 def getData(data):
+    
     host = data.get('host')
     port = data.get('port')
     userName = data.get('username')
@@ -40,43 +38,73 @@ def getData(data):
     isHA = data.get('isHA')
     replica = data.get('replica')
     mysqlPort = data.get('mysqlPort')
+    slave1 = data.get('slave1')
+    slave2 = data.get('slave2')
+    serverID1 = data.get('serverID1')
+    serverID2 = data.get('serverID2')
     
-    list = [host, port, userName, password, isHA, replica, mysqlPort]
+    list = [host, port, userName, password, isHA, replica, mysqlPort, slave1, slave2, serverID1, serverID2]
     
     return list    
+
+# master, slave 서버 연결 확인하는 함수
+def testSSHConnection(data):
     
-def sshCommand(list):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy) # 서버 호스트 키 자동 추가
+    ssh_master = paramiko.SSHClient()
+    ssh_slave1 = paramiko.SSHClient()
+    ssh_slave2 = paramiko.SSHClient()
+    
+    ssh_master.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    ssh_slave1.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    ssh_slave2.set_missing_host_key_policy(paramiko.AutoAddPolicy)
     
     try:
-        # ssh 연결
-        ssh = paramiko.SSHClient()
-        ssh.connect(hostname=list[0], port=list[1], username=list[2], password=list[3])
+        ssh_master.connect(hostname=data.get('host'), port=data.get('port'), username=data.get('username'), password=data.get('password'))
+        ssh_slave1.connect(hostname=data.get('slave1'), port=data.get('port'), username=data.get('username'), password=data.get('password'))
+        ssh_slave2.connect(hostname=data.get('slave2'), port=data.get('port'), username=data.get('username'), password=data.get('password'))
         
-        arg1 = list[0]
-        arg2 = list[1]
-        script_command = f'/root/test.sh {arg1} {arg2}' # 테스트 용도
-        stdin, stdout, stderr = ssh.exec_command(script_command)
+        host = data.get('host')
+        slave1 = data.get('slave1')
+        slave2 = data.get('slave2')
+        script_command_master = f'echo {host}'
+        script_command_slave1 = f'echo {slave1}'
+        script_command_slave2 = f'echo {slave2}'
+        
+        print("Executing command:", script_command_master)
+        print("Executing command:", script_command_slave1)
+        print("Executing command:", script_command_slave2)
+        
+    except: "connection failed"
+    
+    return ssh_master, ssh_slave1, ssh_slave2
+        
+# 자동 설치에 필요한 명령들 실행시키는 함수
+def sshCommand(ssh_master, ssh_slave1, ssh_slave2,list):
+    
+    
+    # 셋 다 동일하게 db 설치부터
+    try:
+        arg1 = list[3]
+        script_command = f'/root/install.sh {arg1}' # 테스트 용도
+        stdout, stderr = ssh_master.exec_command(script_command)
     
         # 실시간으로 출력 읽기
         while not stdout.channel.exit_status_ready():
         # stdout에서 한 줄씩 읽고 출력
             if stdout.channel.recv_ready():
                 output = stdout.channel.recv(1024).decode("utf-8")
-                print(output, end='')  # 실시간 출력
-        # 실행 후 남은 출력 읽기
-        
+                print(output, end='') 
         error = stderr.read().decode("utf-8")
         print(error)
 
         
-        # 성공 시
-        return jsonify("success"), 200
-    except paramiko.AuthenticationException:
-        return jsonify(error="Authentication failed"), 401
+    except: "설치가 제대로 진행되지 않았습니다. 다시 시도해 주세요."
     finally:
-        ssh.close()
+        ssh_master.close()
+        ssh_slave1.close()
+        ssh_slave2.close()
+                
+    return
     
 def setLogFile():
     
